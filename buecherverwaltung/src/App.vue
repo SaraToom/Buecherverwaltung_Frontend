b20abc09c881acd6315dcabcd7e5e740a0d256c9
SaraToom <sara.toom@student.htw-berlin.de>
2026-04-25 16:56:10 +0200

    Vue.js App mit Komponente

---
+++ buecherverwaltung/src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref } from 'vue'
+import BuchItem from './components/BuchItem.vue';
+
+// Simuliert das Backend
+const buecherListe = ref([
+  { id: 1, titel: 'Harry Potter und der Stein der Weisen', autor: 'J.K. Rowling', genre: 'Fantasy', isRead: true
+   },
+  { id: 2, titel: '11 Minuten', autor: 'Paulo Coelho', genre: 'Roman', isRead: false },
+  { id: 3, titel: 'Stolz und Vorurteil', autor: 'Jane Austen', genre: 'Klassiker', isRead: false },
+  { id: 4, titel: 'The Housemaid', autor: 'Freida McFadden', genre: 'Thriller', isRead: true }
+])
+
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Meine Bücherverwaltung</h1>
+    </header>
+
+    <main>
+      <BuchItem 
+        v-for="buch in buecherListe" 
+        :key="buch.id" 
+        :titel="buch.titel" 
+        :autor="buch.autor"
+        :genre="buch.genre"
+        :isRead="buch.isRead"
+      />
+    </main>
+  </div>
+</template>
+
+<style>
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+  font-family: Arial, sans-serif;
+  padding: 20px;
+}
+header {
+  text-align: center;
+  margin-bottom: 30px;
+}
+h1 {
+  color: #D5B895;
+   -webkit-text-stroke: 0.9px white;
+}
+#app {
+  background-color: #EBDDCC;
+}
+</style>
---
+++ buecherverwaltung/src/components/BuchItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+// Props für die Buchinformationen
+defineProps(['titel', 'autor', 'genre', 'isRead'])
+</script>
+
+<template>
+  <div class="buch-karte">
+    <h3>{{ titel }}</h3>
+    <p><strong>Autor:</strong> {{ autor }}</p>
+    <p><strong>Genre:</strong> {{ genre }}</p>
+    <p><strong>Gelesen:</strong> {{ isRead ? 'Ja' : 'Nein' }}</p>
+  </div>
+</template>
+
+<style scoped>
+.buch-karte {
+  border: 2px solid white; 
+  padding: 15px;
+  margin: 10px 0;
+  border-radius: 10px;
+  background-color: #D5B895;
+  color: white;
+}
+</style>
